3a90dcf67071520585592da2869b1b9863ddb3a0
Carbon <Cheeseburgerbbgun@gmail.com>
2022-05-04 14:18:18 -0600

    Basic stucture with a fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,72 @@
-import './App.css';
+import React, { useState, useEffect } from "react"
+import uniqid from 'uniqid';
+
+function SearchBar() {
+  return (
+    <form> 
+      <input type="text" placeholder='Search...' />
+    </form>
+  )
+}
+
+function Post({ title }) {
+
+  return <span> {title} </span>
+
+}
+
+function PostContainer( props ) {
+  const  { posts } = props;
+  
+  if(!posts || posts.length === 0 ) return <p>Blog Posts Loading</p>
+
+  return (
+    <div>
+      {posts.map( post => {
+        return (
+          <Post  title={ post.title } key={ uniqid() } />
+        )
+      })}
+    </div>
+  )
+  
+}
 
 function App() {
+  const [postList, setPostList] = useState({
+    loading: false,
+    posts: []
+  })
+  
+  useEffect(() => {
+
+    async function fetchData() {
+      
+
+      try {
+        let response = await fetch('http://192.168.0.27:3000/api/v/1/public/post?limit=title');
+        let posts = await response.json();
+        setPostList({
+          loading: false,
+          posts: posts
+        });
+
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    
+    fetchData()
+    
+  }, [setPostList])
+  
   return (
     <div className="App">
+
+    <SearchBar />
+
+    <PostContainer posts={postList.posts}/>
+
     </div>
   );
 }
